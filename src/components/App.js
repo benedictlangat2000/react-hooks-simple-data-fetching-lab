055d96f728cfb12d2af1a1cd6a8d0804c17391de
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-08-20 14:58:54 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [Loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch dog image from the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching dog image:', error);
+        setLoading(false);
+      });
+  }, []); // Empty dependency array means this effect runs only once after the initial render
+
+  return (
+    <div className="App">
+      <h1>Random Dog Image</h1>
+      {Loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
